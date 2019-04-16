c648bb6c3b856198a39dabd47ff450558f1e361f
wadhah <wadhah.mahorug15@gmail.com>
2019-04-17 00:20:06 +0100

    Added trees and old TP

---
+++ TP semestre 2/TP 1/EX1.cpp
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct noeud 
+{
+	int val ;
+	struct  noeud* next ; 
+}noeud ; 
+
+typedef noeud *list ;
+
+list creer()
+{
+	return NULL;
+}
+
+list readList(list l , int n)
+{
+
+	if (n)
+		 {
+		 	int x ;
+			scanf("%d",&x);
+		 	l = (list)malloc(sizeof(noeud));
+		 	l->val = x ; 
+		 	l->next  = readList(l->next,n-1);
+		 }
+	else 
+		return NULL ; 
+}
+
+void printList(list l)
+{
+	while (l)
+	{
+      printf("%d ",l->val);
+      l = l -> next  ;
+    }
+	
+}
+
+list insert(list l , int p,int e)
+{
+	int i = 1 ;
+	if (p==1)
+	{
+		list temp ; 
+		temp = (list)malloc(sizeof(noeud));
+		temp -> val = e ;
+		temp -> next = l ;
+		return temp ; 
+	}
+	else 
+	{
+		list temp = l ; 
+		while (i<(p-1) && (temp->next))
+		{
+			i++;
+			temp = temp ->next ; 
+		}
+		if (i < (p-1))
+		{
+			printf("insertion impossible\n");
+			return l ; 
+		}
+		else 
+		{
+		list newlist ;
+		newlist = (list)malloc(sizeof(noeud));
+		newlist->val = e ; 
+		newlist->next = temp -> next ; 
+		temp->next = newlist ;
+		return l ; 
+  	 	}
+  	 }
+	
+}
+list supp(list l , int p)
+{
+	int i = 1 ;
+	if (p==1)
+	{
+		list temp = l->next ; 
+		free(l);
+		return temp ; 
+	}
+	else 
+	{
+		list temp = l ; 
+		while (i<(p-1) && (temp->next->next))
+		{
+			i++;
+			temp = temp ->next ; 
+		}
+		if (i < (p-1))
+		{
+			printf("suppression impossible\n");
+			return l ; 
+		}
+		else 
+		{
+			list temp2 = temp ->next ; 
+			temp ->next = temp -> next -> next ;
+			free(temp2); 
+		return l ; 
+  	 	}
+
+	}
+}
+int listlength(list l)
+{
+	int i = 0 ; 
+	if (l)
+	{
+		while(l)
+		{
+			l = l->next ; 
+			i++ ;
+		}
+	}
+		return i ; 
+}
+int main()
+{
+	freopen("in.txt","r+",stdin);
+	freopen("out.txt","w+",stdout);	
+	list l ; 
+	//lire une liste de n element 
+	l = readList(l,4);
+	l = insert(l,5,90);
+	printList(l);
+	printf("\n");
+	l = supp(l,5);
+	printList(l);
+	printf("\n");
+	printf("la longueur de la liste est %d\n",listlength(l));
+	return 0 ;
+}
---
+++ Trees/Program.c
@@ -0,0 +1,139 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+typedef struct leaf
+{
+    int value ;
+    struct leaf *l, *r ;
+} leaf;
+
+typedef  leaf *tree  ;
+
+
+tree createLeaf(int n)
+{
+    tree t = (tree)malloc(sizeof(leaf)) ;
+    t->value = n ;
+    t->l = NULL ;
+    t->r = NULL ;
+}
+
+int max (int a, int b)
+{
+    if (a > b)
+        return a;
+    return b ;
+}
+
+void preOrderTraversal(tree t)
+{
+    if (!t)
+        return ;
+    printf("%d\n", t->value);
+    preOrderTraversal(t->l);
+    preOrderTraversal(t->r);
+}
+
+void inOrderTraversal(tree t)
+{
+    if (!t)
+        return ;
+    inOrderTraversal(t->l);
+    printf("%d\n", t->value);
+    inOrderTraversal(t->r);
+
+
+}
+void postOrderTraversal(tree t)
+{
+    if (!t)
+        return ;
+    postOrderTraversal(t->l);
+    postOrderTraversal(t->r);
+    printf("%d\n", t->value);
+}
+
+int treeHeight(tree t)
+{
+    if (!t)
+        return 0 ;
+    else
+        return 1 + max(treeHeight(t->l), treeHeight(t->r));
+}
+
+void printlevel(tree t, int n)
+{
+    if (!n)
+        printf("%d | ", t->value);
+    else
+    {
+        if (t->l)
+            printlevel(t->l, n - 1);
+        if (t->r)
+            printlevel(t->r, n - 1);
+    }
+}
+void levelOrderTraversal(tree t)
+{
+    int l = treeHeight(t);
+    for (int i = 0 ; i < l ; i++)
+    {
+        printlevel(t, i);
+        printf("\n");
+    }
+}
+
+tree adjd(tree h, tree t)
+{
+    h->r = t ;
+    return h ;
+}
+
+tree adjg(tree h, tree t)
+{
+    h->l = t ;
+    return h ;
+}
+tree insertIntoBST(tree t, int n)
+{
+    if (n > t->value)
+    {
+        if (!t->r)
+        {
+            t->r = createLeaf(n);
+            return t ;
+        }
+        else
+            t = adjd(t, insertIntoBST(t->r, n)) ;
+    }
+    else if (n < t->value)
+    {
+        if (!t->l)
+        {
+            t->l = createLeaf(n);
+            return t ;
+        }
+        else
+            t = adjg(t, insertIntoBST(t->l, n)) ;
+    }
+}
+int main()
+{
+    freopen("in.txt", "r+", stdin);
+    freopen("out.txt", "w+", stdout);
+    tree t = createLeaf(8);
+    t->l = createLeaf(3);
+    t->r = createLeaf(10);
+    t->r->r = createLeaf(14);
+    t->r->r->l = createLeaf(13);
+    t->l->l = createLeaf(1);
+    t->l->r = createLeaf(6);
+    t->l->r->l = createLeaf(4);
+    t->l->r->r = createLeaf(7);
+    levelOrderTraversal(t);
+    printf("\n");
+    insertIntoBST(t,11);
+    levelOrderTraversal(t);
+
+
+}
