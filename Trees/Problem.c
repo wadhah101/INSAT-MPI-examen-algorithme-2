91461ce7470272f748b40db057b8af858a381e8c
BoogieP <wadhah.mahorug15@gmail.com>
2019-04-17 00:34:27 +0100

    added the lib and the file

--- Trees/Problem.c
+++ Trees/Problem.c
@@ -1,24 +1,22 @@
-#include <bits/stdc++.h>
-#define bint int64_t  ; 
-
-using namespace std ; 
-
-void cinArray(int *a,int n)
-{
-	for (int i = 0 ; i<n ; ++i)
-		cin >> a[i] ; 
-}
-
-void coutArray (int *a,int n)
-{
-	for (int i = 0 ; i<n ; ++i)
-		cout << a[i] ; 
-}
+#include "stdio.h"
+#include "stdlib.h"
+#include "TreesLib.h"
 
 int main()
 {
-	freopen("in.txt","r+",stdin);
-	freopen("out.txt","w+",stdout);
-	std::ios::sync_with_stdio(false);
-	cin.tie(0) ; cout.tie(0) ; 
+    freopen("in.txt", "r+", stdin);
+    freopen("out.txt", "w+", stdout);
+    tree t = createLeaf(8);
+    t->l = createLeaf(3);
+    t->r = createLeaf(10);
+    t->r->r = createLeaf(14);
+    t->r->r->l = createLeaf(13);
+    t->l->l = createLeaf(1);
+    t->l->r = createLeaf(6);
+    t->l->r->l = createLeaf(4);
+    t->l->r->r = createLeaf(7);
+    levelOrderTraversal(t);
+    printf("\n");
+    insertIntoBST(t,11);
+    levelOrderTraversal(t);
 }
