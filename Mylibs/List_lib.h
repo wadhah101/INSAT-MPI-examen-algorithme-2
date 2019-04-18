d4d72767e015526cd55c8d965211e1e0e40a01c3
wadhah <wadhah.mahroug15@gmail.com>
2019-04-18 22:20:46 +0100

    Merged list and pile lib

--- Mylibs/List_lib.h
+++ Mylibs/List_lib.h
@@ -5,7 +5,7 @@ typedef struct node
 {
     int val;
     struct node *next;
-}node ,*list;
+}node ,*list,*pile;
 
 void printlist(list l)
 {
@@ -59,3 +59,162 @@ list readList(list l, int n)
     else
         return l;
 }
+
+
+pile creatPile()
+{
+    return NULL ;
+}
+
+int PileVide(pile p)
+{
+    return p == NULL ;
+}
+
+void piler(pile *p, int n)
+{
+    pile y = (pile)malloc(sizeof(node)) ;
+    y->val = n ;
+    y->next = *p ;
+    *p = y ;
+}
+
+void depiler(pile *p)
+{
+    pile t = *p ;
+    *p = (*p)->next;
+    free(t);
+}
+
+int tetePile(pile p)
+{
+    return p->val ;
+}
+
+int LongeurPile(pile p)
+{
+    int num = 0 ;
+    pile q = creatPile();
+    while(p)
+    {
+        num ++ ;
+        piler(&q, tetePile(p));
+        depiler(&p);
+    }
+    while(q)
+    {
+        piler(&p, tetePile(q));
+        depiler(&q);
+    }
+    return num ;
+}
+
+void printpile(pile p)
+{
+    pile q = creatPile();
+    while(p)
+    {
+        printf("%d ", tetePile(p));
+        piler(&q, tetePile(p));
+        depiler(&p);
+    }
+    while(q)
+    {
+        piler(&p, tetePile(q));
+        depiler(&q);
+    }
+    printf("\n");
+}
+
+//file
+typedef struct file
+{
+    struct node *first, *last ;
+} file;
+
+file creatFile()
+{
+    file temp ;
+    temp.first = NULL ;
+    temp.last = NULL ;
+    return temp ;
+}
+
+int VideFile(file f)
+{
+    return (f.first == NULL && f.last == NULL) ;
+}
+
+void enfiler(file *f, int n)
+{
+    node *temp ;
+    temp = (node *)malloc(sizeof(node));
+    temp->val = n ;
+    temp->next = NULL;
+    if (VideFile(*f))
+    {
+        f->last = temp ;
+        f->first = temp ;
+    }
+    else
+    {
+        f->last->next = temp ;
+        f->last = temp ;
+    }
+}
+
+void defiler(file *f)
+{
+    if (f->first == f->last)
+    {
+        free(f->first);
+        *f = creatFile();
+    }
+    else
+    {
+        node *t = f->first ;
+        f->first = f->first->next ;
+        free(t);
+    }
+}
+
+int teteFile(file f)
+{
+    return f.first->val;
+}
+
+int LongeurFile(file *f)
+{
+    int num = 0 ;
+    file g = creatFile() ;
+    while(!VideFile(*f))
+    {
+        enfiler(&g, teteFile(*f));
+        defiler(f);
+        num ++ ;
+    }
+    while(!VideFile(g))
+    {
+        enfiler(f, teteFile(g));
+        defiler(&g);
+    }
+    return num ;
+}
+
+void printFile(file *f)
+{
+    file g = creatFile() ;
+    while(!VideFile(*f))
+    {
+        printf("%d ", teteFile(*f));
+        enfiler(&g, teteFile(*f));
+        defiler(f);
+    }
+    while(!VideFile(g))
+    {
+        enfiler(f, teteFile(g));
+        defiler(&g);
+    }
+    printf("\n");
+}
+
